10ac8ed170efba23bff58bcc4ab1bec68fa7faec
Christian Martin <cmartin60@academic.rrc.ca>
2025-02-23 20:47:26 -0600

    Edit: edit controller to integrate with the proper function and make sure all errors are passed to middleware instad of manual handling it.

--- src/api/v1/controllers/employeeController.ts
+++ src/api/v1/controllers/employeeController.ts
@@ -5,16 +5,10 @@ import * as employeeService from "../services/employeeService";
 /**
  * @description Create a new employee.
  * @route POST /employees
- * @returns {Promise<void>}
  */
-export const createEmployee = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-): Promise<void> => {
+export const createEmployee = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
         const newEmployee: Employee = await employeeService.createEmployee(req.body);
-
         res.status(201).json({ message: "Employee Created", data: newEmployee });
     } catch (error) {
         next(error);
@@ -24,16 +18,10 @@ export const createEmployee = async (
 /**
  * @description Get all employees.
  * @route GET /employees
- * @returns {Promise<void>}
  */
-export const getAllEmployees = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-): Promise<void> => {
+export const getAllEmployees = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
         const employees: Employee[] = await employeeService.getAllEmployees();
-
         res.status(200).json({ message: "Employees Retrieved", data: employees });
     } catch (error) {
         next(error);
@@ -43,16 +31,11 @@ export const getAllEmployees = async (
 /**
  * @description Get an employee by ID.
  * @route GET /api/v1/employees/:id
- * @returns {Promise<void>}
  */
-export const getEmployeeById = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-): Promise<void> => {
+export const getEmployeeById = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
         const employeeId = req.params.id;
-        const employee: Employee = await employeeService.getEmployeeById(employeeId); 
+        const employee: Employee = await employeeService.getEmployeeById(employeeId);
 
         res.status(200).json({ message: "Employee Retrieved", data: employee });
     } catch (error) {
@@ -63,18 +46,10 @@ export const getEmployeeById = async (
 /**
  * @description Update an existing employee.
  * @route PUT /api/v1/employees/:id
- * @returns {Promise<void>}
  */
-export const updateEmployee = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-): Promise<void> => {
+export const updateEmployee = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-        const updatedEmployee: Employee = await employeeService.updateEmployee(
-            req.params.id,
-            req.body
-        );
+        const updatedEmployee: Employee = await employeeService.updateEmployee(req.params.id, req.body);
         res.status(200).json({ message: "Employee Updated", data: updatedEmployee });
     } catch (error) {
         next(error);
@@ -84,13 +59,8 @@ export const updateEmployee = async (
 /**
  * @description Delete an employee.
  * @route DELETE /api/v1/employees/:id
- * @returns {Promise<void>}
  */
-export const deleteEmployee = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-): Promise<void> => {
+export const deleteEmployee = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
         await employeeService.deleteEmployee(req.params.id);
         res.status(200).json({ message: "Employee Deleted" });
